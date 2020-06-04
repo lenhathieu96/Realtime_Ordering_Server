5abbc7d0bba8f2b00bd2e5e5f6f04f70663b7485
LeeNhaHie <lenhathieu@LeeNhaHies-MacBook-Pro.local>
2020-06-04 15:47:44 +0700

    fix heroku

--- Route/authRoute.js
+++ Route/authRoute.js
@@ -3,6 +3,9 @@ const router = express.Router()
 
 const authController = require('../Controller/authController')
 
+router.get('/',(req,res)=>{
+    res.send('hello to my server')
+})
 router.post('/',authController.Login)
 
 module.exports = router
