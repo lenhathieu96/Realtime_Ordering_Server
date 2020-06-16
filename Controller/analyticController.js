706038c59319f17bca4252eac7785f8ff70b2b43
LeeNhaHie <lenhathieu@LeeNhaHies-MacBook-Pro.local>
2020-06-16 21:02:17 +0700

    update

--- Controller/analyticController.js
+++ Controller/analyticController.js
@@ -11,24 +11,28 @@ module.exports.getWeekRevenue = async (req,res) => {
         for(let i = 0; i<7 ; i++){
             const filterbyDay = bill.filter(item=>new Date(item.Created).getDate()===today-i)
             const revenuebyDay = filterbyDay.reduce((revenue,eachBill)=>revenue+=eachBill.TotalPrice,0)
-            revenue.push({
-                day: i===0?"Hôm Nay":i===1?"Hôm Qua":"Ngày "+(today-i),
-                revenue: revenuebyDay})
+            revenue.push(
+                {
+                    day: i===0?"Hôm Nay":i===1?"Hôm Qua":"Ngày "+(today-i),
+                    revenue: revenuebyDay,
+                    billQuantity: filterbyDay.length
+                }
+            )
         }
         res.status(200).json(revenue)
     })
       
 }
 
-module.exports.getRevenueByMonth = (req,res) => {
+module.exports.getRevenueByMonth = async (req,res) => {
     let month = new Date(Date.now()).getMonth()
-    Bill.find({},(err,bill)=>{
+    await Bill.find({},(err,bill)=>{
         if(err){
             console.log(err)
             res.status(400).json({err})
         }
         const filterbyMonth = bill.filter(item=>new Date(item.Created).getMonth()=== month)
         const revenuebyMonth = filterbyMonth.reduce((revenue,eachBill)=>revenue+=eachBill.TotalPrice,0)
-        res.send({revenue:revenuebyMonth})
+        res.status(200).json({revenue:revenuebyMonth})
     })
 }
